7ff330715f6b0edb06a82fe5799009f9922e189d
andrecodelima <andrelimag89@gmail.com>
2024-09-05 23:35:31 -0300

    add projeto

---
+++ controle-imoveis/src/main/java/br/net/generallinhares/controleimoveis/ControleImoveisApplication.java
@@ -0,0 +1,13 @@
+package br.net.generallinhares.controleimoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleImoveisApplication.class, args);
+	}
+
+}
---
+++ controle-imoveis/src/test/java/br/net/generallinhares/controleimoveis/ControleImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.generallinhares.controleimoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
